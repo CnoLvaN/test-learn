2fac5d451625447b298044ad4f85023b3c33e8fa
CnoLvaN <anordgame@gmail.com>
2024-11-30 22:13:56 +0100

    hooks downloaded

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -2,22 +2,24 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import { PRODUCTS } from "../../../assets/products";
 import { ProductListItem } from "../../components/product-list-item";
 import { ListHeader } from "../../components/list-header";
+import Auth from "../auth";
 // import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <FlatList
-        data={PRODUCTS}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={2}
-        ListHeaderComponent={ListHeader}
-        contentContainerStyle={styles.flatListContent}
-        columnWrapperStyle={styles.flatListColumn}
-        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
-      />
-    </View>
+    <Auth />
+    // <View>
+    //   <FlatList
+    //     data={PRODUCTS}
+    //     renderItem={({ item }) => <ProductListItem product={item} />}
+    //     keyExtractor={(item) => item.id.toString()}
+    //     numColumns={2}
+    //     ListHeaderComponent={ListHeader}
+    //     contentContainerStyle={styles.flatListContent}
+    //     columnWrapperStyle={styles.flatListColumn}
+    //     style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+    //   />
+    // </View>
   );
 };
 
--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,4 +1,6 @@
 import { View, Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import * as zod from "zod";
 
 export default function Auth() {
   return (
